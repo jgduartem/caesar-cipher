654a07489f3c11b17dcb2ec9010a77cb0da8d142
Jose Duarte <joseduarte019@gmail.com>
2020-08-27 08:41:31 -0400

    caesar cipher completed

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,12 +3,28 @@ puts "Caesar Cipher"
 def caesar_cipher(word, number)
 
   letters=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  letters_upcase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N" "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  letters_upcase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   word_cipher=[]
+  i=0
+
   while i<word.length do
-    for x in 0..25
+    for x in 0..26
+      if word[i]==" "
+        word_cipher[i]=" "
+      end
+      if word[i]=="!" || word[i]=="?"
+        word_cipher[i]=word[i]
+      end
+
+      if word[i]==word[i].upcase && word[i]==letters_upcase[x]
+        if x+number<25
+          word_cipher[i]=letters_upcase[x+number]
+        else
+          word_cipher[i]=letters_upcase[number-(26-x)]
+        end
+      end
       if word[i]==letters[x]
-        if (x+number) <25
+        if x+number<25
           word_cipher[i]=letters[x+number]
         else
           word_cipher[i]=letters[number-(26-x)]
@@ -17,5 +33,13 @@ def caesar_cipher(word, number)
     end
     i+=1
   end
-  puts word_cipher.join("")
+  puts word_cipher.join
 end
+
+puts "Introduce the text"
+phrase=gets.chomp
+puts "Introduce the cipher number"
+number=gets.chop.to_i
+caesar_cipher(phrase,number)
+
+
